d8403d46a6372a8a5220e498f612ebb5dd8c853f
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2025-05-09 12:12:19 -0400

    Create types.ts
    Create type definitions

---
+++ docupicks/src/types.ts
@@ -0,0 +1,14 @@
+export interface Movie {
+    Title: string;
+    Year: string;
+    Plot: string;
+    imdbRating: string;
+    Poster: string;
+    Genre: string;
+    imdbID: string;
+    Director: string;
+    Rated: string;
+    Runtime: string;
+    Response?: string;
+    Error?: string;
+}
